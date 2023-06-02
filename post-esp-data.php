04276e1bc004748d24902722a1bae95f4a07186b
Jean Emmanuel Kouadio <75110337+jeanemmanuelk@users.noreply.github.com>
2023-06-02 17:40:14 -0400

    Update post-esp-data.php

--- post-esp-data.php
+++ post-esp-data.php
@@ -1,52 +1,77 @@
 <?php
+/**
+ * Smart Bin Capstone Project - Data Receiver
+ *
+ * This script receives data from an ESP32 device equipped with bin sensors and stores it in a database.
+ * It validates the API key, processes the received data, and inserts it into the database.
+ *
+ * Instructions:
+ * 1. Set up a MySQL database and update the database credentials ($dbname, $dbuser, $dbpass) accordingly.
+ * 2. Replace $api_key_value with your desired API key.
+ * 3. Make sure the ESP32 sketch sends the data using an HTTP POST request to this script's URL.
+ *
+ * @version 1.0
+ * @author Your Name
+ */
 
+// Database credentials
+$dbname = '';       // Your server database name
+$dbuser = '';       // Your server username
+$dbpass = '';       // Your database password
+$dbhost = 'localhost';  // Your database hosting server name ("localhost" for local development)
 
-$dbname = 'kjesendata'; //my server database
-$dbuser = 'ggt6zshwjs19';  //my server username
-$dbpass = 'Kouadio@7'; //database password
-$dbhost = 'localhost'; //database hosting server name "localhost for godaddy"
-
-// Keep this API Key value to be compatible with the ESP32 code provided in the project page. 
+// Keep this API Key value to be compatible with the ESP32 code provided in the project page.
 // If you change this value, the ESP32 sketch needs to match
 $api_key_value = "tPmAT5Ab3j7F9";
 
-$api_key= $distance = $weight = $binState = "";
+// Initialize variables
+$api_key = $distance = $weight = $binState = "";
 
+// Check if the request method is POST
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $api_key = test_input($_POST["api_key"]);
-    if($api_key == $api_key_value) {
+
+    // Validate the API key
+    if ($api_key == $api_key_value) {
         $distance = test_input($_POST["distance"]);
         $weight = test_input($_POST["weight"]);
         $binState = test_input($_POST["binState"]);
-        
-        // Create connection
+
+        // Create database connection
         $conn = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
-        // Check connection
+
+        // Check database connection
         if ($conn->connect_error) {
             die("Connection failed: " . $conn->connect_error);
-        } 
-        
-         $sql = "INSERT INTO BinSensor (distance, weight, binState) VALUES ('" . $distance . "', '" . $weight . "', '" . $binState . "')";
-        
+        }
+
+        // Prepare SQL statement
+        $sql = "INSERT INTO BinSensor (distance, weight, binState) VALUES ('" . $distance . "', '" . $weight . "', '" . $binState . "')";
+
+        // Execute SQL statement
         if ($conn->query($sql) === TRUE) {
             echo "New record created successfully";
-        } 
-        else {
+        } else {
             echo "Error: " . $sql . "<br>" . $conn->error;
         }
-        
-                $conn->close();
-    }
-    else {
+
+        // Close database connection
+        $conn->close();
+    } else {
         echo "Wrong API Key provided.";
     }
-
-}
-else {
+} else {
     echo "No data posted with HTTP POST.";
 }
 
-function test_input($data) {
+/**
+ * Function to sanitize and validate input data
+ *
+ * @param string $data The input data to be processed
+ * @return string The sanitized and validated data
+ */
+function test_input($data)
+{
     $data = trim($data);
     $data = stripslashes($data);
     $data = htmlspecialchars($data);
